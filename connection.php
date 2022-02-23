c87303236d414935c89685a96d8bb31e144859a1
aartidharajiya <95265435+aartidharajiya@users.noreply.github.com>
2022-02-23 13:28:06 +0530

    Update

--- connection.php
+++ connection.php
@@ -2,9 +2,9 @@
 /**************************Connection********************************/
 	
 $connection=new mysqli("localhost","root","","movies");
-if($connection->connect_errno)
+if($connection->connect_errno)  //connect_errno returns error if there any error.
 {
-    echo $connection->connect_error;
+    echo $connection->connect_error; //connect_error returns error description if there if any error ocurs.
     exit;
 }
 ?>
